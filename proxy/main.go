fa2e0dbc5f68c17af85b0c62529545d6aefc66b5
DCchoudhury15 <divyanshuchoudhury3@gmail.com>
2026-01-02 18:48:22 +0530

    feat: add chaos middleware with 50% failure rate and 2s latency

--- proxy/main.go
+++ proxy/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	target, err := url.Parse("https://www.google.com")
 	if err != nil {
 		fmt.Println("Error parsing URL:", err)
@@ -26,4 +29,22 @@ func main() {
 	if err := http.ListenAndServe(":8080", proxy); err != nil {
 		fmt.Printf("Server failed: %v\n", err)
 	}
+}
+
+func ChaosMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if rand.Intn(100) < 50 {
+			fmt.Println(" Chaos Monkey struck! Returning 500 Error.")
+			w.WriteHeader(http.StatusInternalServerError)
+
+			w.Write([]byte("Chaos Monkey: Service unavailable!"))
+
+			return
+		}
+
+		fmt.Println(" Simulating network lag (2 seconds)...")
+		time.Sleep(2 * time.Second)
+
+		next.ServeHTTP(w, r)
+	})
 }
